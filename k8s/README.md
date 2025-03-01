411c1a45d5b45c092594e3aaa99f1d271c915178
Daniel Volinsky <danivoli5@gmail.com>
2025-03-01 13:40:09 +0200

    first cluster successful deployment
    there is issue with redis

---
+++ k8s/README.md
@@ -0,0 +1,45 @@
+# MongoDB Replica Set Configuration
+
+```
+Even with preserved PVCs, sometimes the replica set configuration is not reloaded automatically.
+
+To fix this, you can run the following command:
+
+Exec into the first pod (typically mongo-0):
+kubectl exec -it mongo-0 -- mongo -u mongouser -p mongopass --authenticationDatabase admin
+
+In the shell, initialize the replica set:
+rs.initiate({
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "mongo-0.mongo:27017" },
+    { _id: 1, host: "mongo-1.mongo:27017" },
+    { _id: 2, host: "mongo-2.mongo:27017" }
+  ]
+});
+
+Confirm with:
+rs.status();
+```
+
+# Redis Replica Set Configuration
+
+```
+Exec into the replica pod (e.g., redis-1):
+kubectl exec -it redis-1 -- redis-cli
+
+Then, in the Redis CLI, run:
+REPLICAOF redis-0.redis 6379
+
+Verify Replication:
+On redis-1, run:
+INFO replication
+
+You should see:
+role:slave (or replica)
+master_host: redis-0.redis
+master_link_status: up
+
+Optionally, check redis-0 with:
+kubectl exec -it redis-0 -- redis-cli INFO replication
+```
--- src/main/java/com/handson/tinyurl/config/MongoConfig.java
+++ src/main/java/com/handson/tinyurl/config/MongoConfig.java
@@ -5,34 +5,20 @@ import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoConfig {
 
-    @Value("${spring.data.mongodb.host}")
-    private String host;
-
-    @Value("${spring.data.mongodb.port}")
-    private int port;
+    @Value("${spring.data.mongodb.uri}")
+    private String uri;
 
     @Value("${spring.data.mongodb.database}")
     private String database;
 
-    @Value("${spring.data.mongodb.username}")
-    private String username;
-
-    @Value("${spring.data.mongodb.password}")
-    private String password;
-
     @Bean
     public MongoClient mongoClient() {
-        String connectionString = String.format(
-                "mongodb://%s:%s@%s:%d/%s?authSource=admin",
-                username, password, host, port, database
-        );
-        return MongoClients.create(connectionString);
+        return MongoClients.create(uri);
     }
 
     @Bean
